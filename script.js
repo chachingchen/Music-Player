b3c3457debb2798e1c760ece74e1688fbcd5e6be
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2021-11-24 14:26:03 +0800

    add player controls and progress bar function

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress'); 
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// music
+const songs = [
+	{
+		name: 'jacinto-1',
+		displayName: 'Electric Chill Machine',
+		artist: 'Jacinto Design'
+	},
+	{
+		name: 'jacinto-2',
+		displayName: 'Seven Nation Army',
+		artist: 'Jacinto Design'
+	},
+	{
+		name: 'jacinto-3',
+		displayName: 'Goodnight, Disco Queen',
+		artist: 'Jacinto Design'
+	},
+	{
+		name: 'metric-1',
+		displayName: 'Front Row',
+		artist: 'Jacinto Design'
+	}
+];
 
 // Check if Playing
 let isPlaying = false;
@@ -24,3 +55,75 @@ function pauseSong() {
 
 //Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+	title.textContent = song.displayName;
+	artist.textContent = song.artist;
+	music.src = `music/${song.name}.mp3`;
+	image.src = `img/${song.name}.jpg`;
+}
+
+// current song
+let songIndex = 0;
+
+// previous song
+function prevSong() {
+	songIndex--;
+	if(songIndex < 0) {
+		songIndex = songs.length -1;
+	}
+	// console.log(songIndex);
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// next song
+function nextSong() {
+	songIndex++;
+	if (songIndex > songs.length -1) {
+		songIndex = 0;
+	}
+	// console.log(songIndex);
+	loadSong(songs[songIndex]);
+	playSong();
+}
+// on load= select first song
+loadSong(songs[songIndex]);
+
+// update Progress Bar & time
+
+function updateProgressBar(e) {
+  if(isPlaying) {
+  	const {duration, currentTime} = e.srcElement;
+  	// update progress bar width
+  	const progressPercent = (currentTime / duration) *100;
+  	progress.style.width = `${progressPercent}%`;
+  	// calculate displey for duration
+  	const durationMinutes = Math.floor(duration / 60);
+  	console.log('minutes: ', durationMinutes);
+  	let durationSeconds = Math.floor(duration % 60);
+  	if(durationSeconds < 10) {
+  		durationSeconds = `0${durationSeconds}`
+  	}
+  	console.log('seconds: ', durationSeconds);
+  	
+  	// Delay switching duration element to avoid NaN
+  	if(durationSeconds) {
+  		durationEl.textContent = `${durationMinutes}:${durationSeconds}`; 
+  	}
+  	// calculate displey for currentTime
+  	const currentMinutes = Math.floor(currentTime / 60);
+  	console.log('minutes: ', currentMinutes);
+  	let currentSeconds = Math.floor(currentTime % 60);
+  	if(currentSeconds < 10) {
+  		currentSeconds = `0${currentSeconds}`
+  	}
+  	console.log('seconds: ', currentSeconds);
+  	currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`; 
+  }
+}
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
